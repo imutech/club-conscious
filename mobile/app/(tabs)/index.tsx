c6b62582c1f8bed38fe4e56cb7a0098d655ec8e2
sain0136 <55252053+sain0136@users.noreply.github.com>
2025-03-13 21:42:20 -0400

    Refactor Home screen layout and integrate background image styling

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -12,38 +12,19 @@ import { theme } from "@/src/theme/theme";
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
-      <ImageBackground source={backgroundImageConfig} style={}>
+      <ImageBackground source={backgroundImageConfig} style={styles.background}>
         #TODO: Add Toast for API connection failed
-        {/* <Toast
-        visible={useGlobalStore.getState().apiConnectionFailed}
-        position={Toast.positions.CENTER}
-      >
-        We're experiencing technical difficulties. Please try again later.
-      </Toast> */}
-        <View style={styles.container}>
-          <Text style={styles.text}>Home screen</Text>
-          Go to About screen
-          <Link href="/(tabs)/settings" style={styles.button}></Link>
-        </View>
       </ImageBackground>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  background: theme.appBackgroundImage,
+  background: theme.appBackgroundImage as any,
   container: {
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
     justifyContent: "center",
-  },
-  text: {
-    color: "#fff",
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#fff",
   },
 });
